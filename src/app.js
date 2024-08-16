8e8286c80c090a9c33b31f0f7365e8736b42f963
pravin <sharmapravin7631@gmail.com>
2024-08-16 16:04:46 +0530

    folder structure change

--- src/app.js
+++ src/app.js
@@ -7,9 +7,12 @@ const Registration = require("./models/registrations")
 
 const port = process.env.PORT || 3000;
 
+// const static_path = path.join(__dirname,"../public");
+// const template_path = path.join(__dirname,"../templates/views");
+// const partials_path = path.join(__dirname,"../templates/partials");
 const static_path = path.join(__dirname,"../public");
-const template_path = path.join(__dirname,"../templates/views");
-const partials_path = path.join(__dirname,"../templates/partials");
+const template_path = path.join(__dirname,"./templates/views");
+const partials_path = path.join(__dirname,"./templates/partials");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
